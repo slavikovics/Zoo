8f40e01838e2508a78a309152e4c249b18605873
slavikovics <144949100+slavikovics@users.noreply.github.com>
2025-09-24 01:30:38 +0300

    Fixing DataGrid visualization

--- Zoo/ViewModels/PetsViewModel.cs
+++ Zoo/ViewModels/PetsViewModel.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -10,18 +13,42 @@ namespace Zoo.ViewModels;
 public partial class PetsViewModel : ViewModelBase
 {
     private readonly ISelectService<Animal> _selectService;
-    
+
     public ObservableCollection<Animal> Animals { get; set; }
     
     public PetsViewModel(ISelectService<Animal> selectService)
     {
         _selectService = selectService;
-        LoadAnimals();
+        Animals = new ObservableCollection<Animal>();
+        Task.Run(LoadAnimals);
     }
 
     private async Task LoadAnimals()
     {
-        var animals = await _selectService.SelectAll("Animals");
-        if (animals != null) Animals = new ObservableCollection<Animal>(animals);
+        try
+        {
+            var animals = await _selectService.SelectAll("Animals");
+            var animalsList = animals?.ToList();
+
+            // Обновляем коллекцию в UI потоке
+            await Avalonia.Threading.Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                Animals.Clear();
+                if (animalsList != null)
+                {
+                    foreach (var animal in animalsList)
+                    {
+                        Animals.Add(animal);
+                    }
+                }
+            });
+
+            Console.WriteLine($"Loaded {Animals.Count} animals"); // Для отладки
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error loading animals: {ex.Message}");
+            // Можно добавить обработку ошибок
+        }
     }
 }
