42f20195d3115e510916481443e606fa7e9862e0
slavikovics <144949100+slavikovics@users.noreply.github.com>
2025-10-23 01:36:01 +0300

    Joining all dropdown calls in one method

--- Zoo/ViewModels/AnimalEditViewModel.cs
+++ Zoo/ViewModels/AnimalEditViewModel.cs
@@ -27,14 +27,13 @@ public partial class AnimalEditViewModel : ViewModelBase
     private ObservableCollection<AnimalType> _animalTypes = [];
 
     [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(Animal))]
-    private ObservableCollection<BirdsWinterPlace?> _winterPlaces = [BirdsWinterPlace.Empty()];
+    private ObservableCollection<BirdsWinterPlace> _winterPlaces = [BirdsWinterPlace.Empty()];
 
     [ObservableProperty] 
-    private ObservableCollection<ReptileInfo?> _reptileInfos = [ReptileInfo.Empty()];
+    private ObservableCollection<ReptileInfo> _reptileInfos = [ReptileInfo.Empty()];
 
     [ObservableProperty] 
-    private ObservableCollection<Diet?> _diets = [Diet.Empty()];
+    private ObservableCollection<Diet> _diets = [Diet.Empty()];
 
     [ObservableProperty] 
     private ObservableCollection<HabitatZone> _habitatZones = [];
@@ -47,12 +46,15 @@ public partial class AnimalEditViewModel : ViewModelBase
 
     [ObservableProperty] 
     private ObservableCollection<Employee> _selectedVets = [];
+    
+    [ObservableProperty]
+    private ObservableCollection<Employee> _vetsToRemove = [];
 
     [ObservableProperty] 
     private Employee? _selectedVetToAdd;
 
     [ObservableProperty] 
-    private string? _error;
+    private string _error = "";
 
     public ObservableCollection<string> SexOptions { get; } = ["Male", "Female", "Unknown"];
 
@@ -65,119 +67,32 @@ public partial class AnimalEditViewModel : ViewModelBase
 
     private async Task InitializeAsync()
     {
-        await Task.WhenAll(LoadAnimalTypes(),
-            LoadBirdsWinterPlaces(),
-            LoadReptileInfos(),
-            LoadDiets(),
-            LoadHabitatZones(),
-            LoadCaretakers());
+        await Task.WhenAll(
+            LoadDropdown("AnimalTypes", AnimalTypes),
+            LoadDropdown("BirdsWinterPlaces", WinterPlaces),
+            LoadDropdown("ReptilesInfo", ReptileInfos),
+            LoadDropdown("Diets", Diets),
+            LoadDropdown("HabitatZones", HabitatZones),
+            LoadDropdown("Employees", Caretakers));
+
+        AvailableVets = Caretakers;
     }
 
-    private async Task LoadAnimalTypes()
+    private async Task LoadDropdown<T>(string tableName, ObservableCollection<T> targetCollection) where T: class
     {
         try
         {
-            var types = await _dataService.SelectAll<AnimalType>("AnimalTypes");
+            var items = await _dataService.SelectAll<T>(tableName);
             
-            if (types is null) return;
-            foreach (var type in types)
+            if (items is null) return;
+            foreach (var item in items)
             {
-                AnimalTypes.Add(type);
+                targetCollection.Add(item);
             }
         }
-        catch (Exception ex)
+        catch (Exception e)
         {
-            Error += ex.Message;
-        }
-    }
-    
-    private async Task LoadBirdsWinterPlaces()
-    {
-        try
-        {
-            var places = await _dataService.SelectAll<BirdsWinterPlace>("BirdsWinterPlaces");
-            
-            if (places is null) return;
-            foreach (var place in places)
-            {
-                WinterPlaces.Add(place);
-            }
-        }
-        catch (Exception ex)
-        {
-            Error += ex.Message;
-        }
-    }
-    
-    private async Task LoadReptileInfos()
-    {
-        try
-        {
-            var info = await _dataService.SelectAll<ReptileInfo>("ReptilesInfo");
-            
-            if (info is null) return;
-            foreach (var reptileInfo in info)
-            {
-                ReptileInfos.Add(reptileInfo);
-            }
-        }
-        catch (Exception ex)
-        {
-            Error += ex.Message;
-        }
-    }
-    
-    private async Task LoadDiets()
-    {
-        try
-        {
-            var diets = await _dataService.SelectAll<Diet>("Diets");
-            
-            if (diets is null) return;
-            foreach (var diet in diets)
-            {
-                Diets.Add(diet);
-            }
-        }
-        catch (Exception ex)
-        {
-            Error += ex.Message;
-        }
-    }
-    
-    private async Task LoadHabitatZones()
-    {
-        try
-        {
-            var habitats = await _dataService.SelectAll<HabitatZone>("HabitatZones");
-            
-            if (habitats is null) return;
-            foreach (var habitat in habitats)
-            {
-                HabitatZones.Add(habitat);
-            }
-        }
-        catch (Exception ex)
-        {
-            Error += ex.Message;
-        }
-    }
-    
-    private async Task LoadCaretakers()
-    {
-        try
-        {
-            var caretakers = await _dataService.SelectAll<Employee>("Employees");
-            
-            if (caretakers is null) return;
-            foreach (var caretaker in caretakers)
-            {
-                Caretakers.Add(caretaker);
-            }
-        }
-        catch (Exception ex)
-        {
-            Error += ex.Message;
+            Error += e.Message;
         }
     }
 
@@ -186,13 +101,20 @@ public partial class AnimalEditViewModel : ViewModelBase
     }
 
     [RelayCommand]
-    private void AddVet()
+    private void AddVet(Employee newVet)
     {
+        if (!SelectedVets.Contains(newVet)) SelectedVets.Add(newVet);
     }
 
     [RelayCommand]
     private void RemoveVet()
     {
+        var vetsToRemoveList = VetsToRemove.ToList();
+        
+        foreach (var vet in vetsToRemoveList)
+        {
+            SelectedVets.Remove(vet);
+        }
     }
 
     [RelayCommand]
